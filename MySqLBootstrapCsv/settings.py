52f8a4a76b4e5c5f0bd17648087471cb980d3bfb
stockkates <77533909+stockkates@users.noreply.github.com>
2021-01-27 14:27:05 +0100

    Update settings.py
    you have to add password into settings.py

--- MySqLBootstrapCsv/settings.py
+++ MySqLBootstrapCsv/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'ko54YK66',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
