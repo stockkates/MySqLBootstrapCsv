64e82241a21fa7ccb5dd9d452762adf6d76eed58
stockkates <77533909+stockkates@users.noreply.github.com>
2021-02-23 18:21:28 +0100

    Update settings.py

--- MySqLBootstrapCsv/settings.py
+++ MySqLBootstrapCsv/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x1((3lbc@wiz0-@+@_pe(mi2@o8$1gke)0@*fe)w7m=$p4h&2z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
